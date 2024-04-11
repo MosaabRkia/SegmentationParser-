c966b355b2cce9ed19559ff35a20cec10f1818a4
Mosaab Abo Rkia <aborkiamosaab@gmail.com>
2024-04-12 02:54:38 +0300

    test

--- dataset/downloader_dataset.py
+++ dataset/downloader_dataset.py
@@ -1,28 +1,19 @@
 import os
 import gdown
-from rarfile import RarFile, NotRarFile
 
 # Function to download file from Google Drive
 def download_file(file_id, output_filename):
     url = f'https://drive.google.com/uc?id={file_id}'
+    print(f"Downloading from: {url}")
+    print(f"To: {output_filename}")
     gdown.download(url, output_filename, quiet=False)
-
-# Function to extract files from RAR archive
-def extract_rar(archive_path, output_dir):
-    try:
-        with RarFile(archive_path) as rar:
-            rar.extractall(output_dir)
-    except NotRarFile:
-        print(f"{archive_path} is not a RAR file.")
+    print("Download completed.")
 
 # File IDs and output filenames
 file_ids = ["1Y_39YXWHtKIHvSe_p3GDdGSO07Q-rpOv"]  # Replace with your file IDs
-output_filenames = ["file1.rar", "file2.rar", "file3.rar"]  # Output filenames for downloaded files
+output_filenames = ["file1.rar"]  # Output filenames for downloaded files
 
 # Download files
 for file_id, output_filename in zip(file_ids, output_filenames):
+    print(f"Downloading file with ID: {file_id}")
     download_file(file_id, output_filename)
-
-# Extract files from downloaded archives
-for filename in output_filenames:
-    extract_rar(filename, os.path.splitext(filename)[0])  # Extract to a directory with the same name as the RAR file (without extension)
